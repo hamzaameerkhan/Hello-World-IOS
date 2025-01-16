7cc4072988d6a81a435e2edb34380b5787fab3df
DanishCh4321 <Danishjamil4321@gmail.com>
2025-01-16 08:50:20 -0500

    Initial commit for Hello World iOS project

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'hello-world-ios' do
+  use_frameworks!
+
+  # Pods for hello-world-ios
+  pod 'Alamofire'
+end
---
+++ Tests/hello-world-iosTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import hello_world_ios
+
+class hello_world_iosTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertTrue(true, "This test always passes.")
+    }
+}
